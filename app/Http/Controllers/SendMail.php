23795dd034bae31e3ff785ed3bb1846c117b7e03
gustavosantaella <gasantaella@gmail.com>
2021-02-01 01:06:05 -0400

    aja

--- app/Http/Controllers/SendMail.php
+++ app/Http/Controllers/SendMail.php
@@ -16,7 +16,7 @@ class SendMail extends Controller
         $mail->SMTPAuth   = true; // authentication enabled
         $mail->SMTPSecure = 'ssl'; // secure transfer enabled REQUIRED for Gmail
         $mail->Host       = "smtp.gmail.com";
-        $mail->Port       = 465; // or 587
+        $mail->Port       = 587; // or 587
         $mail->IsSMTP();
         $mail->IsHTML(true);
         $mail->Username = "theskinpower.ca@gmail.com";
